52a65f36b6ef558f6f7ea4831ae7e6519030e646
unbounddev <unbounddev@git>
2025-12-03 16:04:42 -0800

    Add DictNode to ast when bracket exists and add parse_entry function

--- lua/parse.lua
+++ lua/parse.lua
@@ -1,25 +1,13 @@
 local KEYS = {
   NULL = 0,
   SPACE = 32,
+  DOUBLE_QUOTE = 34,
+  SINGLE_QUOTE = 39,
   LEFT_BRACKET = 123,
   RIGHT_BRACKET = 125
 }
 
 
-local function parse_ws(file, row, i, j)
-  while i <= row do
-    while j <= #file[i] do
-      local char_code = string.byte(file[i], j)
-      if char_code == KEYS.SPACE then
-      else
-        return i, j            
-      end
-      j = j + 1
-    end
-    i = i + 1
-  end
-  return i, j
-end
 
 local function parse_manifest(file, pos)
   local row, col = unpack(pos)
@@ -50,17 +38,62 @@ local function parse_manifest(file, pos)
     return ch
   end
 
+  local function parse_ws()
+    while i <= row do
+      while j <= #file[i] do
+        local char_code = string.byte(file[i], j)
+        if char_code == KEYS.SPACE then
+        else
+          return             
+        end
+        j = j + 1
+      end
+      i = i + 1
+    end
+  end
+
+  local function parse_string()
+    return false
+  end
+
+  local function parse_entry()
+    while i <= row do 
+      while j <= #file[i] do
+        -- handle whitespace
+        parse_ws()
+        if end_of_parse() then
+          return
+        end
+        -- parse option/key
+        local char = peek()
+        if char == KEYS.DOUBLE_QUOTE or char == KEYS.SINGLE_QUOTE then
+          local option = parse_string()
+          if not option then
+            return false
+          end
+        end
+      end
+    end
+  end
+
   -- handle whitespace
-  i, j = parse_ws(file, row, i, j) 
+  parse_ws() 
   if end_of_parse() then
     return suggestions
   end
+
+  -- handle left bracket
   if peek() ~= KEYS.LEFT_BRACKET then
     return suggestions
   else 
     -- parse LEFT_BRACKET
     next()
+    table.insert(ast, { entries = {} }) 
   end
+
+  -- parse entries
+  repeat
+  until end_of_parse() or not parse_entry()
   print("parse complete")
 end
 
